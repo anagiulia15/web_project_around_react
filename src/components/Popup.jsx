fb22777431acf11ce73d05abe6549c58cc6a00db
ana giulia <anaazanedo09@gmail.com>
2024-11-16 13:02:13 -0500

    se agrega componente popup

---
+++ src/components/Popup.jsx
@@ -0,0 +1,26 @@
+export default function Popup({
+  children,
+  name,
+  title,
+  handleClose,
+  handleSubmit,
+  isOpen
+}) {
+  return (
+    <div className={`popup popup_${name} ${isOpen ? 'popup_show' : ''}`}>
+      <div className="popup__overlay" onClick={handleClose}></div>
+      <div className="popup__content">
+        <button className="popup__close" onClick={handleClose}></button>
+        <div className="popup__body">
+          <h2 className="popup__title">{title}</h2>
+          <form className={`form form_${name}`} onSubmit={handleSubmit}>
+            {children}
+            <fieldset className="form__set">
+              <input type="submit" className="form__submit" value="Guardar" />
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
