f7090bba72ea10579d1201f62787d139428672f9
ana giulia <anaazanedo09@gmail.com>
2024-11-16 13:06:49 -0500

    se agrega componente popup

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import api from "../utils/api";
 import Card from "./Cards";
 import { currentuserContext } from "../contexts/CurrentUserContext";
+import Popup from "./Popup";
 
 export default function Main(props) {
   const currentUser = useContext(currentuserContext);
@@ -21,37 +22,7 @@ export default function Main(props) {
           );
         })}
       </main>
-
-      <div className="popup" id="popupopenimage">
-        <div className="popup__overlay"></div>
-        <div className="popup__content">
-          <button className="popup__close"></button>
-          <div className="popup__body popup__body_transparent">
-            <img src="images/element1.jpg" alt="" className="popup__image" />
-            <h2 className="popup__subtitle"></h2>
-          </div>
-        </div>
-      </div>
-
-      <div className="popup popup_confirmation">
-        <div className="popup__overlay"></div>
-        <div className="popup__content">
-          <button className="popup__close"></button>
-          <div className="popup__body">
-            <img src="images/element1.jpg" alt="" className="popup_image" />
-            <h2 className="popup__title">¿Estas seguro?</h2>
-            <form className="form form_confirmation">
-              <fieldset className="form__set">
-                <input
-                  type="submit"
-                  className="form__submit"
-                  value="Si, Guardar"
-                />
-              </fieldset>
-            </form>
-          </div>
-        </div>
-      </div>
+      <Popup />
     </div>
   );
 }
