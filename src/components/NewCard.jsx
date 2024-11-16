a27d3eb2504db4978c7e7d5e2b2863ea93cff43b
ana giulia <anaazanedo09@gmail.com>
2024-11-16 13:00:39 -0500

    ajute de nombre

--- src/components/NewCard.jsx
+++ src/components/NewCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import PopupWithForm from "./PopupWithForm";
 
-export default function newCard({
+export default function NewCard({
   isEditProfilePopup,
   handleClosePopups,
 
